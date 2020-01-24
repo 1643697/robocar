fac4aba5bc9c815080550963dbd745fd96cb8375
1643697 <55006299+1643697@users.noreply.github.com>
2020-01-24 12:52:04 -0500

    vid stream

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>RoboGuard</title>
+        <meta charset="UTF-8">
+        <h1> 
+            hello
+        </h1>
+
+        <link rel="stylesheet" href="Style.css">
+    </head>
+
+    <body>
+        <h1>Car Cam</h1> 
+
+      <iframe width=”1080” height=”720” src="/html/min.php" width="640" height="480" frameborder="0" allowfullscreen></iframe> 
+
+    
+        <br>
+
+        <a href="next.php">Cat facts</a>
+        <br>
+        <a href="email.php">Email Sign up</a>
+        <br>
+
+      </body>
+</html>
