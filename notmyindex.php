16e313449e2a14059cbb3e806f52c26eb1dfa55d
1643697 <55006299+1643697@users.noreply.github.com>
2020-01-28 15:32:06 -0500

    Update notmyindex.php
    only what i need

--- notmyindex.php
+++ notmyindex.php
@@ -20,33 +20,6 @@
 	        <div class="container-fluid text-center liveimage">
          <div><img id="mjpeg_dest" onclick="toggle_fullscreen(this);" src="cam_pic.php?time=1580237211861&amp;pDelay=16666"></div>
       </div>
-      
-      <div class="container-fluid text-center">
-         <div class="panel-group" id="accordion">
-            <div class="panel panel-default">
-               <div class="panel-heading">
-                  <h2 class="panel-title">
-                     <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne">Camera Settings</a>
-                  </h2>
-               </div>
-               <div id="collapseOne" class="panel-collapse collapse">
-                  <div class="panel-body">
-                     <table class="settingsTable">
-                        <tbody><tr>
-                           <td>Resolutions:</td>
-                           <td>Load Preset: <select onchange="set_preset(this.value)">
-								                                 <option value="1920 1080 25 25 2592 1944">Select option...</option>
-                                 <option value="1920 1080 25 25 2592 1944">Full HD 1080p 16:9</option>
-                                 <option value="1280 0720 25 25 2592 1944">HD-ready 720p 16:9</option>
-                                 <option value="1296 972 25 25 2592 1944">Max View 972p 4:3</option>
-                                 <option value="768 576 25 25 2592 1944">SD TV 576p 4:3</option>
-                                 <option value="1920 1080 01 30 2592 1944">Full HD Timelapse (x30) 1080p 16:9</option>
-								                               </select><br>
-                       
-               </div>
-            </div>
-         </div>
-      </div>
-         
+ 
 
 </body></html>
