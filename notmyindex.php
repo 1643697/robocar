b5f84a20ad084c74a36a295ff4ff7fb488d19835
1643697 <55006299+1643697@users.noreply.github.com>
2020-01-28 15:36:32 -0500

    Update notmyindex.php
    sdf

--- notmyindex.php
+++ notmyindex.php
@@ -10,16 +10,11 @@
 
 
    <body onload="setTimeout('init(0, 60, 1);', 100);">
-      <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-         <div class="container">
-            <div class="navbar-header">
-               <a class="navbar-brand" href="#">RPi Cam Control v6.6.7: mycam@raspberrypi</a>
-            </div>
-         </div>
-      </div>
-	        <div class="container-fluid text-center liveimage">
+      
+	   <div class="container-fluid text-center liveimage">
          <div><img id="mjpeg_dest" onclick="toggle_fullscreen(this);" src="cam_pic.php?time=1580237211861&amp;pDelay=16666"></div>
       </div>
- 
 
-</body></html>
+   </body>
+
+</html>
