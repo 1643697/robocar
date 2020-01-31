35f96a274df694d3d0843dd2717545f43d53e878
1643697 <55006299+1643697@users.noreply.github.com>
2020-01-31 11:18:48 -0500

    Added button press

--- myindex.php
+++ myindex.php
@@ -13,6 +13,34 @@
       <h1>Car Cam</h1> 
       <div><img id="mjpeg_dest" /></div>
     </center>
+
+    <?php
+        if(array_key_exists('button1', $_POST)) { 
+            button1(); 
+        } 
+        else if(array_key_exists('button2', $_POST)) { 
+            button2(); 
+        } 
+        function button1() { 
+            echo "This is Button1 that is selected"; 
+            $output = `ls -lart`;
+            echo "<div>$output</div>";
+        } 
+        function button2() { 
+            echo "This is Button2 that is selected"; 
+        } 
+    ?> 
+  
+    <form method="post"> 
+        <input type="submit" name="button1"
+                class="button" value="Button1" /> 
+          
+        <input type="submit" name="button2"
+                class="button" value="Button2" /> 
+    </form> 
+
+
+
  </body>
 </html>
 
