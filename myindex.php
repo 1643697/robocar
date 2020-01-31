73241aeecf32e2aacbd991e109097ec71f62fa85
1643697 <55006299+1643697@users.noreply.github.com>
2020-01-31 10:25:32 -0500

    main page with js

--- myindex.php
+++ myindex.php
@@ -1,41 +1,19 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>RoboGuard</title>
-        <meta charset="UTF-8">
-        <link rel="stylesheet" href="Style.css">
 
+  <head>
+    <title>RoboGuard</title>
+    <meta charset="UTF-8">
+      <link rel="stylesheet" href="Style.css">
+    <script src="script_min.js"></script>
+  </head>
 
-    </head>
-
-    <body onload="setTimeout('init(0, 60, 1);', 100);">
-        <h1>Car Cam</h1> 
-
-     
-
-      <iframe src="/html/min.php" style="border: 0; width: 100%; height: 100%" scrolling="no"> </iframe>
-    
-      <div><img id="mjpeg_dest" onclick="toggle_fullscreen(this);" src="cam_pic.php?time=1580237946234&amp;pDelay=16666" class=""></div>
-
-      <div class="container-fluid text-center liveimage">
-         <div><img id="mjpeg_dest" onclick="toggle_fullscreen(this);" src="cam_pic.php?time=1580239409150&amp;pDelay=16666" class=""></div>
-         <div id="main-buttons">
-            <input id="video_button" type="button" class="btn btn-primary" value="record video start">
-            <input id="image_button" type="button" class="btn btn-primary" value="record image">
-            <input id="timelapse_button" type="button" class="btn btn-primary" value="timelapse start">
-            <input id="md_button" type="button" class="btn btn-primary" value="motion detection start">
-            <input id="halt_button" type="button" class="btn btn-danger" value="stop camera">
-         </div>
-      </div>
-      
-        <br>
-
-        <a href="next.php">Cat facts</a>
-        <br>
-        <a href="email.php">Email Sign up</a>
-        <br>
-
-      </body>
+  <body onload="setTimeout('init();', 100);">
+    <center>
+      <h1>Car Cam</h1> 
+      <div><img id="mjpeg_dest" /></div>
+    </center>
+ </body>
 </html>
 
 
