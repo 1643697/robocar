57283a95520b66ae7ab60555b442382ab3eb48db
1643697 <55006299+1643697@users.noreply.github.com>
2020-02-11 14:50:36 -0500

    565

--- scripts.js
+++ scripts.js
@@ -18,12 +18,13 @@ function init() {
 
 
 function Recordalert() {
-
+  var d = new Date();
   document.getElementById("p1").innerHTML = "Recording Started     ";
   document.getElementById("p2").innerHTML = d; 
 }
 
 function FinishedRecordalert() {
+  var d = new Date();
   document.getElementById("p1").innerHTML = "Recording Stopped     ";
   document.getElementById("p2").innerHTML = d; 
 }
